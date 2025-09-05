9dced45e03b8b63d36e158c2e5dd39103a18c852
Lucas Emanuel <lucas.emanueldamata@gmail.com>
2025-09-05 20:53:04 -0300

    Add files via upload

---
+++ Previsão do Tempo/script.js
@@ -0,0 +1,34 @@
+let chave = "9269f5af89716500f45f1993a91a1e37";
+
+function clique() {
+    let cidade = document.querySelector(".input-cidade").value
+    
+    buscarCidade(cidade);
+
+}
+
+//https://api.openweathermap.org/data/2.5/weather?q=   {city name}&appid={API key}
+
+async function buscarCidade(cidade) {
+    let dados = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q="+
+        cidade +
+        "&appid=" +
+        chave +
+        "&lang=pt_br" +
+        "&units=metric"
+        )
+
+        .then(res => res.json())
+
+        console.log(dados);
+
+        colocaNaTela(dados);
+
+        
+
+}
+
+function colocaNaTela(dados){
+    document.querySelector(".nome-cidade").innerHTML = dados.name;
+} 
